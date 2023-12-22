42913135a8301e3dc091b3a24ef6013ee3c2eb74
Johnny3140 <Johnnylopez0314@gmail.com>
2023-12-21 21:56:42 -0500

    started mongo connection for test

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const mongoose = require('mongoose')
 
 
 //Connection String 
+const connectionString = '';
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
