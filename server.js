47d80f9dbf05a588c2bae7097c8733059cd604c9
Johnny3140 <Johnnylopez0314@gmail.com>
2023-12-23 15:42:20 -0500

    products added to database

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const productRoutes = require('./routes/products.js');
 const productCtrl = require('./controllers/productController');
+const Product = require('./models/product.js')
 // Create an Express application
 const app = express();
 
@@ -22,6 +23,39 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
+const productsData = [
+    {
+      "name": "blonde roast",
+      "price": 12,
+      "stock": 56,
+      "description": "Blonde-roasted coffee beans have a shorter roast time, allowing for an easy-drinking cup of more mellow flavors.",
+      "image": "https://circlehousecoffee.com/wp-content/uploads/2023/06/mahaba-light-roast-coffee-bean-circle-house-coffee-fort-lauderdale-img2.jpg"
+    },
+    {
+      "name": "medium roast",
+      "price": 12,
+      "stock": 56,
+      "description": "Medium-roasted coffee beans are smooth and balanced, with rich, approachable flavors.",
+      "image": "https://circlehousecoffee.com/wp-content/uploads/2023/06/mahaba-light-roast-coffee-bean-circle-house-coffee-fort-lauderdale-img2.jpg"
+    },
+    {
+      "name": "dark roast",
+      "price": 12,
+      "stock": 56,
+      "description": "Dark-roasted coffees have a fuller body with robust, bold taste.",
+      "image": "https://circlehousecoffee.com/wp-content/uploads/2023/06/mahaba-light-roast-coffee-bean-circle-house-coffee-fort-lauderdale-img2.jpg"
+    }
+  ];
+
+  async function insertProducts() {
+    try {
+      const insertedProducts = await Product.insertMany(productsData);
+      console.log('Products added:', insertedProducts);
+    } catch (error) {
+      console.error('Error adding products:', error);
+    }
+  }
+
 // Use product routes
 app.use('/products', productRoutes);
 
@@ -32,4 +66,3 @@ app.listen(PORT, () => {
 });
 
 
-//test test?
