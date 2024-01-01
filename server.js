5065054b2078e8d9bb2a2dcdf1f1c9c08d610b68
rebeccaloaustin <rebeccaloaustin@gmail.com>
2024-01-01 12:37:39 -0500

    backend connection issues

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const productRoutes = require('./routes/products.js');
 const productCtrl = require('./controllers/productController');
-const Product = require('./models/product.js');
-const Order = require('./models/order.js')
-const User = require('../models/user')
+// const Product = require('./models/product.js');
+// const Order = require('./models/order.js')
+const User = require('./routes/user')
 const orderRoutes = require('./routes/orderRoutes.js')
 // const initializeDatabase = require("./initializeDatabase")
 const methodOverride = require('method-override');
@@ -16,6 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 // Middleware to parse JSON requests
 // app.use(bodyParser.json());
+app.use(cors())
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -23,7 +24,7 @@ mongoose.connect(process.env.MONGODB_URI, {
   })
     .then(() => {
       console.log('Connected to MongoDB');
-      initializeDatabase(); // Call the initialization function
+      // initializeDatabase(); // Call the initialization function
       startServer();
     })
     .catch((error) => {
